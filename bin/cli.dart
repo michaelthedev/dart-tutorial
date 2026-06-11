645e11bffc7c9432dd548e9c1f25aace5924315a
Michael A. <39175160+michaelthedev@users.noreply.github.com>
2026-06-11 14:21:37 +0100

    include http request to weather api

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'package:http/http.dart' as http;
 
 void main(List<String> arguments) {
   if (arguments.isEmpty || arguments.first == 'help') {
@@ -17,7 +18,7 @@ void printUsage() {
   print("Usage: reply with 'help', or 'weather {location}'");
 }
 
-void getWeather(String? location) {
+void getWeather(String? location) async {
   final String inputLocation;
 
   if (location == null) {
@@ -33,5 +34,23 @@ void getWeather(String? location) {
   }
 
   print('One sec. Getting weather data...');
-  print('The weather in $inputLocation is sunny!');
+  final res = await fetchWeatherData(inputLocation);
+  print(res);
+  // print('The weather in $inputLocation is sunny!');
+}
+
+Future<String> fetchWeatherData(String location) async {
+  final response = await http.get(
+      Uri.http(
+        'api.weatherapi.com',
+        '/v1/current.json',
+        {'key': '', 'q': location},
+      )
+  );
+
+  if (response.statusCode == 200) {
+    return response.body;
+  } else {
+    throw Exception('Failed to load weather data. Status code: ${response.statusCode}');
+  }
 }
